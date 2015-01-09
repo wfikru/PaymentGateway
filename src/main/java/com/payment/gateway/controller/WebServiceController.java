cbebfe23771cb57d0aea927a84037bb5e675bbf4
wfikru <fkrabebe@gmail.com>
2015-01-08 22:20:58 -0600

    encryption added

--- src/main/java/com/payment/gateway/controller/WebServiceController.java
+++ src/main/java/com/payment/gateway/controller/WebServiceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.payment.gateway.service.RestService;
+import com.payment.gateway.service.encryptionService;
 
 @Controller
 public class WebServiceController {
@@ -16,15 +17,26 @@ public class WebServiceController {
 	@Autowired
 	private RestService service;
 
+	@Autowired
+	private encryptionService encryptor;
+	
 	public void setService(RestService service) {
 		this.service = service;
 	}
 
 	@RequestMapping("/validate")
 	public @ResponseBody String Validate(
-			@RequestParam("ccn") String creditCardNumber,
+			@RequestParam("ccn") String encCreditCardNumber,
 			@RequestParam("amount") double amount) {
 
+		String creditCardNumber = null;
+		try {
+			creditCardNumber = encryptor.decrypt(encCreditCardNumber);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
+				
 		CreditCard creditCard = null;
 		List<CreditCard> cards = service.checkCard(creditCardNumber);
 		
---
+++ src/main/java/com/payment/gateway/service/encryptionService.java
@@ -0,0 +1,40 @@
+package com.payment.gateway.service;
+
+import java.security.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.*;
+
+public class encryptionService {
+
+	private static final String ALGO = "AES";
+	private static final byte[] keyValue = new byte[] { 'f', 'i', 'k', 'r',
+			'u', 'a', 'b', 'e', 'b', 'e','1','2','3','4','5','6' };
+
+	public static String encrypt(String Data) throws Exception {
+		Key key = generateKey();
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(Cipher.ENCRYPT_MODE, key);
+		byte[] encVal = c.doFinal(Data.getBytes());
+		String encryptedValue = new BASE64Encoder().encode(encVal);
+		return encryptedValue;
+	}
+
+	public static String decrypt(String encryptedData) throws Exception {
+		Key key = generateKey();
+		Cipher c = Cipher.getInstance(ALGO);
+		c.init(Cipher.DECRYPT_MODE, key);
+		byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+		byte[] decValue = c.doFinal(decordedValue);
+		String decryptedValue = new String(decValue);
+		return decryptedValue;
+	}
+
+	private static Key generateKey() throws Exception {
+		Key key = new SecretKeySpec(keyValue, ALGO);
+		return key;
+	}
+
+}
